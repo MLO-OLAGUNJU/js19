23a7adf7b174977ec79b7e31984f732b61321b73
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2023-12-21 16:19:20 +0100

    javascript error handling

--- js/main.js
+++ js/main.js
@@ -5,5 +5,40 @@
 variable = "Manny";
 console.log(variable);*/
 
-//Syntax Error
-Object..create();
+/* //Syntax Error... Bcoz i added two dots (unexpected token ".")
+Object..create(); */
+
+/* // Type Error... Assigning a const twice
+const myName = "Manny";
+myName = "MLO"; */
+
+const makeError = () => {
+  let i = 1;
+  while (i <= 5) {
+    try {
+      if (i % 2 !== 0) {
+        throw new error("Odd number!");
+      }
+      console.log("Even number!");
+      /* const myName = "Manny";
+      myName = "MLO"; */
+    } catch (err) {
+      // console.warn(err);
+      // console.table(err);
+      console.error(err.stack); //give all of error detail.. i like this
+      // console.error(err.name);
+      // console.error(err.message);
+      // logTheError(err.stack);
+    } finally {
+      console.log("........ finally");
+      i++;
+    }
+  }
+};
+makeError();
+
+function customError(message) {
+  this.message = message;
+  this.name = "customError";
+  this.stack = `${this.name}: ${this.message}`;
+}
